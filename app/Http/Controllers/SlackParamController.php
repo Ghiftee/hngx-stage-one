9cb7e419c1e33ece4d46c0465a2ef298f608bf40
Ghiftee <gigibetine@gmail.com>
2023-09-09 18:14:20 +0100

    Fix: Correct time format

--- app/Http/Controllers/SlackParamController.php
+++ app/Http/Controllers/SlackParamController.php
@@ -18,7 +18,7 @@ class SlackParamController extends Controller
         $currentUtcTime = Carbon::now('UTC');
         $currentUtcTime->addMinutes(2); 
         $currentUtcTime->subMinutes(2);
-        $currentUtcTimeFormatted = $currentUtcTime->toIso8601ZuluString();
+        $currentUtcTimeFormatted = $currentUtcTime->toIso8601String();
         
 
         return response()->json([
