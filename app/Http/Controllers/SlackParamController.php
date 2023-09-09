4d8ff5e2bc69816bf6c87d3067fb2266dd287388
Ghiftee <gigibetine@gmail.com>
2023-09-09 09:32:54 +0100

    Fix: Add github url

--- app/Http/Controllers/SlackParamController.php
+++ app/Http/Controllers/SlackParamController.php
@@ -15,17 +15,17 @@ class SlackParamController extends Controller
         // Get the current day of the week
         $currentDayOfWeek = Carbon::now()->format('l');
 
-        // Get the current UTC time with validation of +/-2 hours
         $currentUtcTime = Carbon::now('UTC');
         $currentUtcTime->addMinutes(2); 
         $currentUtcTime->subMinutes(2);
+        
 
         return response()->json([
             'slack_name' => $slackName,
             'current_day_of_week' => $currentDayOfWeek,
             'current_utc_time' => $currentUtcTime->toDateTimeString(),
             'track' => $track,
-            'github_url_of_file' => 'https://github.com/your-repo/file-url',
+            'github_url_of_file' => 'https://github.com/Ghiftee/hngx-stage-one/blob/main/app/Http/Controllers/SlackParamController.php',
             'github_url_of_source_code' => 'https://github.com/Ghiftee/hngx-stage-one',
         ], 200);
 
